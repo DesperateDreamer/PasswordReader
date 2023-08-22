500a84ac7dce531293ef2d473dee3415aa41fa57
Ivanka Kirinova <kiriva2207@gmail.com>
2023-08-22 17:14:42 +0300

    Add the reader and counter for valid passwords

--- PasswordReader/PasswordReader/Program.cs
+++ PasswordReader/PasswordReader/Program.cs
@@ -1,10 +1,15 @@
-﻿namespace PasswordReader
+﻿using System.Text.RegularExpressions;
+
+namespace PasswordReader
 {
     internal class Program
     {
         static void Main(string[] args)
         {
+            //use any name of file
+            string fileName = "file.txt";
 
+            Reader.ReadTheFile(fileName);
         }
     }
 }
